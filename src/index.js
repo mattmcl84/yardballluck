d9c5705f47af19169eeba0519bc8434ee2c46ab9
mattmcl84 <matt_s_mclaughlin@hotmail.com>
2024-05-13 20:46:26 -0400

    Added two titles asa way to practice modifying, staging and committing files

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
       <Title />
+      <Title />
       <RawDataTable />
       <RankingDataTable />
     </React.StrictMode>
